f6e20c31f137648beecd8f253c96a59f254b9731
vetleino <vetle.leinonen-roeim@finn.no>
2014-09-13 23:42:27 +0200

    Consumer group ID is now randomly generated, so that multiple browsers will get all messages.

--- client/src/main/resources/scripts/main.js
+++ client/src/main/resources/scripts/main.js
@@ -11,8 +11,19 @@ connection.onopen = function () {
     //connection.send('Hey server, whats up?');
     connection.send(
         msgpack.encode({command: "init",
-            groupId: "testing-group-id",
+            groupId: makeid(),
             topic: "testing-topic"}))
+}
+
+// Source: http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 }
 
 connection.onclose = function () {
